0e12a6f4b903fa2b3ae884dafc8ebdd77aebd700
GrogDeFrog <george.knightly@gmail.com>
2024-07-18 18:36:00 -0700

    lmao this code is crazy

--- getIntFromUser.cpp
+++ getIntFromUser.cpp
@@ -17,9 +17,9 @@ int getIntFromUser(std::string variableName) {
     
     // Check characters to make sure they're numbers
     for(int i = 1; i < userInput.size(); ++i) {
-      if(!charIsNumber(userInput.at(i))) {	// If not a number
-        out = 0;				// Reset output
-        enteredInt = false;				// Restart the loop
+      if(!charIsNumber(userInput.at(i))) {  // If not a number
+        out = 0;                            // Reset output
+        enteredInt = false;                 // Restart the loop
         break;
       }
       out += pow(10, userInput.size() - i - 1) * (userInput.at(i) - 48);
@@ -30,7 +30,7 @@ int getIntFromUser(std::string variableName) {
       if(charIsNumber(userInput.at(0))) {
         out += pow(10, userInput.size() - 1) * (userInput.at(0) - 48);
       } else if(userInput.at(0) == 45) {
-        out *= -1;	// Sets input to negative if first character is a '-'
+        out *= -1; // Sets input to negative if first character is a '-'
       } else {
         out = 0;
         enteredInt = false;
